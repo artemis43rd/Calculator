e67f944b3563daf6b103fc826643528ec2508361
kkkrrisss <93007425+kkkrrisss@users.noreply.github.com>
2023-12-25 07:02:29 +0300

    Update

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
+
 def on_button_click(button_text):
-    current_text = label.cget("text")
-    
+    current_text = expression_var.get()
+
     # Запрет на ввод первым символом символы операций
     if not current_text and button_text in '+.*/^':
         return
-    
+
     # Запрет на введение двух и более символов операций
     if button_text in '+-*/^.' and current_text and current_text[-1] in '+-*/^.':
         on_delete()
@@ -16,17 +17,18 @@ def on_button_click(button_text):
     elif button_text == '±':
         on_unary_minus()
         return
-    
+
     # Ограничение на количество цифр в текущем операнде
     if button_text.isdigit() and '.' not in current_text:
         operand = current_text.split('+')[-1].split('-')[-1].split('*')[-1].split('/')[0]
         if len(operand.replace('-', '')) >= 15:
             return
-    
+
     current_text += button_text
 
-    label.config(text=current_text)
-    
+    expression_var.set(current_text)
+
+
 def on_key_press(event):
     key = event.char
     if key.isdigit() or key in ['+', '-', '*', '/', '.', '^', '(', ')']:
@@ -35,31 +37,37 @@ def on_key_press(event):
         on_delete()
     elif event.keysym == 'Return':
         on_calculate()
-        
+    root.update()
+
+
 def on_clear():
-    label.config(text="")
+    expression_var.set("")
+
 
 def on_delete():
-    current_text = label.cget("text")
-    label.config(text=current_text[:-1])
+    current_text = expression_var.get()
+    expression_var.set(current_text[:-1])
+
 
 def on_calculate():
-    infix_expression = label.cget("text")
+    infix_expression = expression_var.get()
     try:
         result = eval(infix_expression.replace('^', '**'))
-        label.config(text=str(result))
+        expression_var.set(str(result))
     except Exception as e:
         messagebox.showerror("Error", "Invalid expression")
-        label.config(text="")
+        expression_var.set("")
+
 
 def on_unary_minus():
-    current_text = label.cget("text")
+    current_text =expression_var.get()
 
     # Добавляем унарный минус только если текущее выражение не пусто
     if current_text and not current_text.startswith('-'):
-        label.config(text='-' + current_text)
+        expression_var.set('-' + current_text)
     elif current_text and current_text.startswith('-'):
-        label.config(text=current_text[1:])
+        expression_var.set(current_text[1:])
+
 
 # Создаем основное окно
 root = tk.Tk()
@@ -71,7 +79,8 @@ canvas.grid(row=0, column=0, columnspan=4, pady=5)
 canvas.create_rectangle(0, 0, 400, 40, outline='gray', fill='gray')
 
 # Виджет для отображения текста (метка) с отступом
-label = ttk.Label(root, text="", font=('Arial', 14))
+expression_var = tk.StringVar()
+label = ttk.Label(root, textvariable=expression_var, font=('Arial', 14))
 label.grid(row=0, column=0, columnspan=4, sticky='w', padx=10)
 
 # Кнопки для цифр и операций
@@ -86,7 +95,8 @@ row_val = 1
 col_val = 0
 
 for button in buttons:
-    ttk.Button(root, text=button, command=lambda b=button: on_button_click(b)).grid(row=row_val, column=col_val, padx=5, pady=5)
+    ttk.Button(root, text=button, command=lambda b=button: on_button_click(b)).grid(row=row_val, column=col_val, padx=5,
+                                                                                    pady=5)
     col_val += 1
     if col_val > 3:
         col_val = 0
@@ -101,7 +111,7 @@ ttk.Button(root, text='=', command=on_calculate).grid(row=5, column=3, padx=5, p
 ttk.Button(root, text='±', command=on_unary_minus).grid(row=5, column=2, padx=5, pady=5)
 
 # Привязываем событие нажатия клавиши к функции on_key_press
-root.bind('<Key>', on_key_press)
+root.bind('<KeyPress>', on_key_press)
 
 # Запускаем цикл событий
 root.mainloop()
